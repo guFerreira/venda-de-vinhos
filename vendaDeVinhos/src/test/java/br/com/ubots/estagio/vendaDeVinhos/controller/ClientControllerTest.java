7477cf6738139b53eca6effc9bb45e734e8256c5
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-07 01:08:07 -0300

    Teste controller que verifica o sucesso para o retorno de um cliente com maior compra do ano

---
+++ vendaDeVinhos/src/test/java/br/com/ubots/estagio/vendaDeVinhos/controller/ClientControllerTest.java
@@ -0,0 +1,40 @@
+package br.com.ubots.estagio.vendaDeVinhos.controller;
+
+import ch.qos.logback.core.net.server.Client;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClientControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ClientService clientService;
+
+    @Test
+    public void testReturnClientWithPurchaseBiggestInTheLastYear() throws Exception {
+        Client client = new Client();
+        client.setId(1);
+        client.setName("Gustavo");
+        client.setCpf("000.000.000-01");
+
+        Mockito.when(clientService.getClientWithPurchaseBiggestInTheLastYear())
+                .thenReturn(client);
+
+        mvc.perform(get("/client/purchase"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(containsString("Gustavo")));
+    }
+}
