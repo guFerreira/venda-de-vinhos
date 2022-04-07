1f097807041dc868806890ba754fb9ea5ef333ae
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-07 00:31:25 -0300

    Criação do projeto

---
+++ vendaDeVinhos/src/main/java/br/com/ubots/estagio/vendaDeVinhos/VendaDeVinhosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ubots.estagio.vendaDeVinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaDeVinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaDeVinhosApplication.class, args);
+	}
+
+}
---
+++ vendaDeVinhos/src/test/java/br/com/ubots/estagio/vendaDeVinhos/VendaDeVinhosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ubots.estagio.vendaDeVinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaDeVinhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
